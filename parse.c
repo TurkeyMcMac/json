16a6378ca7e295a889e3fd8557b43def8caf540a
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-03-25 16:08:52 -0400

    Completely remove debug printing from parse.c

--- parse.c
+++ parse.c
@@ -18,7 +18,6 @@ int refill(char **buf, size_t *size, void *ctx)
 
 int main(int argc, char *argv[])
 {
-	int tab = 2;
 	struct json_reader rdr;
 	struct json_item item;
 	FILE *file;
@@ -38,44 +37,6 @@ int main(int argc, char *argv[])
 	do {
 		json_read_item(&rdr, &item);
 		if (item.type > JSON_BOOLEAN) return item.type;
-#if 0
-		printf("%*.c", tab, ' ');
-		if (item.key.bytes) {
-			printf("\"%.*s\": ", (int)item.key.len, item.key.bytes);
-		}
-		switch (item.type) {
-		case JSON_NULL:
-			printf("null,\n");
-			break;
-		case JSON_MAP:
-			printf("{\n");
-			++tab;
-			break;
-		case JSON_END_MAP:
-			printf("\b},\n");
-			--tab;
-			break;
-		case JSON_LIST:
-			printf("[\n");
-			++tab;
-			break;
-		case JSON_END_LIST:
-			printf("\b],\n");
-			--tab;
-			break;
-		case JSON_STRING:
-			printf("\"%.*s\",\n", (int)item.val.str.len,
-				item.val.str.bytes);
-			break;
-		case JSON_NUMBER:
-			printf("%f,\n", item.val.num);
-			break;
-		case JSON_BOOLEAN:
-			printf("%s,\n", item.val.boolean ? "true" : "false");
-			break;
-		default:break;
-		}
-#endif
 	} while (item.type != JSON_EMPTY);
 	return 0;
 }
